fa093102848988a42cff62c7d25b77bc77dc05cf
OttoKto <118813620+OttoKto@users.noreply.github.com>
2024-06-25 15:46:23 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+mnemonics = ["knock", "father", "rice", "child", "fault", "vacant", "viable", "gym", "rocket", "vocal", "spatial", "spot", "earth", "cereal", "verify", "moment", "month", "salad", "design", "sketch", "rude", "hill", "advance", "lift"]
+
